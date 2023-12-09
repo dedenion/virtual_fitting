56e365579959542b7577daf85a639a7c5f3fe6fc
dedenion <m328mitsuya@outlook.jp>
2023-12-10 02:25:08 +0900

    g

--- background_removal/settings.py
+++ background_removal/settings.py
@@ -131,6 +131,6 @@ CLOUDINARY_STORAGE = {
 # Celery設定
 # Celeryの設定
 CELERY_BROKER_URL = os.environ.get('CLOUDAMQP_URL', 'amqp://')  # CloudAMQPの接続URLを環境変数から取得
-CELERY_RESULT_BACKEND = 'rpc://'  # CloudAMQPを結果バックエンドとして指定
+CELERY_RESULT_BACKEND = 'django-db'   # CloudAMQPを結果バックエンドとして指定
 # task状態が開始になったかを確認できるための設定
 CELERY_TASK_TRACK_STARTED = True
